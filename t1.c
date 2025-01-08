39c67f7e7e03d1461d123413fc25e8dc686fb27c
RAKESH <sampathraddyas@gmail.com>
2025-01-08 12:02:21 +0530

    Update t1.c

--- t1.c
+++ t1.c
@@ -1,10 +1,1 @@
 Rakesh is a good boy 
-hbgcjkawrycbkwucBWHWHGHK CBWJKWNJWCB L
-NXWBEKWHCJWbcwy bxcyx jhxc2ui
-xnuzwyebxejbfejnmk kcnlqk
-nuiicfrnjqc nlh qrlcm rh
-xuiqcriercfnhhqdfjkcnrh4
-mqqqqqqqqqqqqqqqUCRUQJEC JERNHVUECBNUEl
-hhshsdhcjn klicn griun co  
-NXKRCGNUR3
-HI MAM MERGE CONFLICT RESOLVED AND CORRECT OUT PUT
